2c43b3a3b7c7c2804c8ab8161c90def0be96ee06
ayushi jaiswal <ayushijaiswal2507@gmail.com>
2023-09-21 18:58:06 +0530

    Added about,home & contact function

--- app.js
+++ app.js
@@ -16,6 +16,23 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+let posts = [];
+
+app.get("/", function (req, res) {
+  res.render("home", {
+    startingContent: homeStartingContent,
+    posts: posts,
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
